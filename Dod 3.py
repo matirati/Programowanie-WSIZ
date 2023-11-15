cbe85aa635a41043493430ba83b398e44980ee32
matirati <matii08@op.pl>
2023-11-15 13:29:13 +0100

    Poprawka 3

--- Dod 3.py
+++ Dod 3.py
@@ -20,12 +20,21 @@ print("4.Dzielenie")
 
 Wybór=input("Wpisz numer działania  (1,2,3,4):")
 
-if Wybór>"4":
-        print("Niepoprawna wartość")
-        sys.exit(0)
+if Wybór != "1" and Wybór!="2" and Wybór!="3" and Wybór!="4":
+    print("Niepoprawna wartość")
+    sys.exit(0)
 
-pierwsza = int(input("Podaj pierwsza liczbe:"))
-druga = int(input("Podaj druga liczbe:"))
+
+
+#if Wybór>"4":
+        #print("Niepoprawna wartość")
+        #sys.exit(0)
+try:
+    pierwsza = int(input("Podaj pierwsza liczbe:"))
+    druga = int(input("Podaj druga liczbe:"))
+except ValueError:
+    print("Nieprawidłowa wartość")
+    sys.exit(1)
 
 if Wybór=="1":
     print(pierwsza,"+",druga,"=", dodawanie(pierwsza, druga))
